21f21a8ff38dda41b44c7d0d68d7dfad1f244376
LuisFernando <zdinhoff@gmail.com>
2025-01-28 20:36:33 -0300

    finalizando

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ document.getElementById('prev').addEventListener('click', () => {
 document.getElementById('next').addEventListener('click', () => {
     showSlide(currentSlide + 1); // Vai para a próxima imagem
 });
+
+const dataInicio = new Date("2024-08-23T00:00:00");
+
+function atualizarContadores(){
+    const agora = new Date();
+    const diferenca = agora - dataInicio;
+
+const anos = Math.floor(diferenca / (1000 * 60 * 60 * 24 * 365));
+const meses = Math.floor((diferenca % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30));
+const dias = Math.floor((diferenca % (1000 * 60 * 60 * 24 * 30) / (1000 * 60 * 60 * 24)));
+const horas = Math.floor((diferenca % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
+const minutos = Math.floor((diferenca % (1000 * 60 * 60)) / (1000 * 60));
+const segundos = Math.floor((diferenca % (1000 * 60)) / 1000);
+
+document.getElementById("ano").innerText = `${anos}\nanos`;
+document.getElementById("mes").innerText = `${meses}\nmeses`;
+document.getElementById("dias").innerText = `${dias}\ndias`;
+document.getElementById("horas").innerText = `${horas}\nhoras`;
+document.getElementById("minutos").innerText = `${minutos}\nminutos`;
+document.getElementById("segundos").innerText = `${segundos}\nsegundos`;
+}
+setInterval(atualizarContadores,1000);
